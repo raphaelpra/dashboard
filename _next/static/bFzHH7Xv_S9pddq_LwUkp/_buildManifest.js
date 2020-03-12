8d7622dbe2c8a5e9c299d52c956964a99e44e8e5
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-03-12 16:41:05 +0100

    Updates

---
+++ _next/static/bFzHH7Xv_S9pddq_LwUkp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a]}}("static\u002Fchunks\u002F2c796e83.2878972929898381b3c3.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
